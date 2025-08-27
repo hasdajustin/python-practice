2b9c3ad6f3ce40c9cfbf7b6065e775783a415eeb
Amrito Ben Hasda <matheusjustinhasda@gmail.com>
2025-08-28 04:43:35 +0600

    create simple calculation method using class

---
+++ calculator.py
@@ -0,0 +1,15 @@
+class Calculator:
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        return a + b
+
+    def divide(self, a, b):
+        return a / b
+
+# Example usage
+if __name__ == "__main__":
+    calc = Calculator()
+    print(calc.add(5, 3))
+    print(calc.divide(10, 2)) 
